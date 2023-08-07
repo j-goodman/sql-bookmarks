000d5079d9a050fa768f432bafd5665976bb52a8
j-goodman <johngoodman@pursuit.org>
2023-08-07 19:36:22 -0400

    Fix config.

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -11,18 +11,18 @@ const cn = {
 
 const db = pgp(cn);
 
-// console.log("Preparing to connect...")
-// db.connect()
-// .then((obj) => {
-//     console.log("Connecting...")
-//     // Can check the server version here (pg-promise v10.1.0+):
-//     const serverVersion = obj.client.serverVersion;
-//     console.log("Postgres connection established");
-//     obj.done(); // success, release the connection;
-//   })
-//   .catch((error) => {
-//     console.log("Connection failed.")
-//     console.log("ERROR:", error.message || error);
-//   });
+console.log("Preparing to connect...")
+db.connect()
+.then((obj) => {
+    console.log("Connecting...")
+    // Can check the server version here (pg-promise v10.1.0+):
+    const serverVersion = obj.client.serverVersion;
+    console.log("Postgres connection established");
+    obj.done(); // success, release the connection;
+  })
+  .catch((error) => {
+    console.log("Connection failed.")
+    console.log("ERROR:", error.message || error);
+  });
 
 module.exports = db;
